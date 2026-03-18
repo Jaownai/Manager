1e1b91816eac1273162992544f10e1c3dfe10431
แจ๋วนาย <131768359+Jaownai@users.noreply.github.com>
2026-03-19 01:08:28 +0700

    Update and rename plus.lua to Sailor.lua

---
+++ Sailor.lua
@@ -0,0 +1,116 @@
+local QuestManager = {
+    ["Thief Hunter"] = {
+        NPCs = "QuestNPC1"
+        Location = "Starter"
+        Mob = "Thief",
+        Level = 0,
+        IsBosses = false
+    },
+    ["Thief Boss"] = {
+        NPCs = "QuestNPC2"
+        Location = "Starter"
+        Mob = "ThiefBoss",
+        Level = 100,
+        IsBosses = true
+    },
+    ["Monkey Hunter"] = {
+        NPCs = "QuestNPC3"
+        Location = "Jungle"
+        Mob = "Monkey",
+        Level = 250,
+        IsBosses = false
+    },
+    ["Monkey Boss"] = {
+        NPCs = "QuestNPC4"
+        Location = "Jungle"
+        Mob = "MonkeyBoss",
+        Level = 500,
+        IsBosses = true
+    },
+    ["Desert Bandit Hunter"] = {
+        NPCs = "QuestNPC5",
+        Location = "Desert",
+        Mob = "DesertBandit",
+        Level = 750,
+        IsBosses = false
+    },
+    ["Desert Bandit Boss"] = {
+        NPCs = "QuestNPC6"
+        Location = "Desert",
+        Mob = "DesertBoss",
+        Level = 1000,
+        IsBosses = true
+    },
+    ["Frost Rogue Hunter"] = {
+        NPCs = "QuestNPC7"
+        Location = "Snow",
+        Mob = "FrostRogue",
+        Level = 1500,
+        IsBosses = false
+    },
+    ["Winter Warden Boss"] = {
+        NPCs = "QuestNPC8"
+        Location = "Snow",
+        Mob = "SnowBoss",
+        Level = 2000,
+        IsBosses = true
+    },
+    ["Sorcerer Hunter"] = {
+        NPCs = "QuestNPC9"
+        Location = "Shibuya",
+        Mob = "Sorcerer",
+        Level = 3000,
+        IsBosses = false
+    },
+    ["Panda Sorcerer Boss"] = {
+        NPCs = "QuestNPC10"
+        Location = "Shibuya",
+        Mob = "PandaMiniBoss",
+        Level = 4000,
+        IsBosses = true
+    },
+    ["Hollow Hunter"] = {
+        NPCs = "QuestNPC11"
+        Location = "HuecoMundo",
+        Mob = "Hollow",
+        Level = 5000,
+        IsBosses = false
+    },
+    ["Strong Sorcerer Hunter"] = {
+        NPCs = "QuestNPC12"
+        Location = "Shinjuku",
+        Mob = "StrongSorcerer",
+        Level = 4000,
+        IsBosses = false
+    },
+    ["Curse Hunter"] = {
+        NPCs = "QuestNPC13"
+        Location = "Shinjuku",
+        Mob = "Curse",
+        Level = 7000,
+        IsBosses = false
+    },
+    ["Slime Warrior Hunter"] = {
+        NPCs = "QuestNPC14"
+        Location = "Slime",
+        Mob = "Slime",
+        Level = 8000,
+        IsBosses = false
+    },
+    ["Blade Masters"] = {
+        NPCs = "QuestNPC16"
+        Location = "Judgement",
+        Mob = "Swordsman",
+        Level = 10000,
+        IsBosses = false
+    },
+    ["Quincy Purge"] = {
+        NPCs = "QuestNPC17"
+        Location = "SoulSociety",
+        Mob = "Quincy",
+        Level = 11000,
+        IsBosses = false
+    },
+}
+
+return QuestManager
